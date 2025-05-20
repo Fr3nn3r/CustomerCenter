17c46f21713adb2496c9ddea1bae097805e61d25
Fred Brunner <frdbrunner@gmail.com>
2025-05-20 12:23:44 +0200

    Tidying up files

--- outreach/config.py
+++ outreach/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 if not DATABASE_URL:
-    raise ValueError('DATABASE_URL not set in environment')
+    raise ValueError("DATABASE_URL not set in environment")
